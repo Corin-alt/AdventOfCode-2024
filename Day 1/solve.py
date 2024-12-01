51ce450bad7c60a6c330a730b23883526022850b
Corentin <dup.corentin51@gmail.com>
2024-12-01 17:07:54 +0100

    add day 1

--- Day 1/solve.py
+++ Day 1/solve.py
@@ -2,13 +2,10 @@ left_array = []
 right_array = []
 
 def read_input(filename='entry.txt'):
-    with open(filename, 'r') as file:
-        left_array, right_array = zip(*[
-            (int(val_g), int(val_d)) for line in file 
-            for val_g, val_d in [line.strip().split()]
-        ])
-    
-    return sorted(left_array), sorted(right_array)
+    with open(filename) as file:
+        left, right = zip(*[map(int, line.split()) for line in file])
+        
+    return sorted(left), sorted(right)
 
 if __name__ == '__main__':
     left_array, right_array = read_input()
