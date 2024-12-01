cd2c9f3e6988b6d7b76a0f1024aa1a03968740c1
Corentin <dup.corentin51@gmail.com>
2024-12-01 17:05:32 +0100

    optimize day 1

--- Day 1/solve.py
+++ Day 1/solve.py
@@ -10,27 +10,14 @@ def read_input(filename='entry.txt'):
     
     return sorted(left_array), sorted(right_array)
 
-def calculate_total_distance(left_array, right_array):
+if __name__ == '__main__':
+    left_array, right_array = read_input()
+    
     if len(left_array) != len(right_array):
         raise ValueError("Arrays must have the same length")
     
-    return sum(abs(left - right) for left, right in zip(left_array, right_array))
-
-
-def calculate_similarity(left_array, right_array):
-    if len(left_array) != len(right_array):
-        raise ValueError("Arrays must have the same length")
-
-    return sum(val * right_array.count(val) for val in left_array)
-
-
-def main():
-    left_array, right_array = read_input()
-    total_distance = calculate_total_distance(left_array, right_array)
-    similarity = calculate_similarity(left_array, right_array)
+    total_distance = sum(abs(left - right) for left, right in zip(left_array, right_array))
+    similarity = sum(val * right_array.count(val) for val in left_array)
 
     print(f'Distance = {total_distance}')
     print(f'Similarité = {similarity}')
-
-if __name__ == '__main__':
-    main()
